39f2cb8dd1bf1ce6d644ef5be2164450251237a9
d4rkd0s <d4rkd0s@d4rkd0s.com>
2019-08-06 22:28:55 -0700

    Updates still not functioning right

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
     require('dotenv').config({path: __dirname + '/.env'})
     const argv = require('yargs').argv
-
+    
     function delay(timeout) {
         return new Promise((resolve) => {
             setTimeout(resolve, timeout);
@@ -20,35 +20,75 @@ const puppeteer = require('puppeteer');
     await page.click('#header-search-text');
     await page.type('#header-search-text', argv.search);
     await page.click('#header-search > button');
-    const nodeChildren = await page.$eval('#thumbs > section > ul', (uiElement) => {
-        return uiElement.children;
-    });
-    console.log(nodeChildren); // Outputs the array of the nodes children
-    nodeChildren.forEach(child => {
-        console.log(child.src);
-    });
+    // await loadAnotherPage(page);
+    // const nodeChildren = await page.$eval('#thumbs > section > ul', (uiElement) => {
+    //     return uiElement.children;
+    // });
 
+    // Wait for the results page to load and display the results.
+    const resultsSelector = '#thumbs';
+    await page.waitForSelector(resultsSelector);
+
+
+    const links = await page.evaluate(resultsSelector => {
+        const anchors = Array.from(document.querySelectorAll(resultsSelector));
+        return anchors.map(anchor => {
+        const title = anchor.textContent.split('|')[0].trim();
+            return `${title} - ${anchor.href}`;
+        });
+    }, resultsSelector);
+    console.log(links.join('\n'));
+
+    // const ul = await page.evaluate(() => document.querySelector('#thumbs > section > ul'));
+    // const imageLis = await page.evaluate(() => {
+    //     const lis = Array.from(document.querySelectorAll('#thumbs > section > ul'));
+    //     return lis;
+    //     const images = lis.map(lis => lis.src);
+    //     return images;
+    // });
+    // console.log(ul);
+    // for (let imageLi of imageLis) {
+    //     console.log(imageLi);
+    // };
+
+    // console.log(nodeChildren); // Outputs the array of the nodes children
+    
     console.log("Closing in 3 seconds");
     await delay(3000);
-    await browser.close();
+    // await browser.close();
 })();
 
-async function autoScroll(page){
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 500;
-            var speed = 50;
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
+async function getImageFromLI(element) {
 
-                if(totalHeight >= scrollHeight){
-                    clearInterval(timer);
-                    resolve();
+}
+
+async function loadAnotherPage(page, scrollStep = 250, scrollDelay = 100) {
+    const lastPosition = await page.evaluate(
+        async (step, delay) => {
+            const getScrollHeight = (element) => {
+            const { scrollHeight, offsetHeight, clientHeight } = element
+            return Math.max(scrollHeight, offsetHeight, clientHeight)
+        }
+        
+        const position = await new Promise((resolve) => {
+            let count = 0
+            const intervalId = setInterval(() => {
+                const { body } = document
+                const availableScrollHeight = getScrollHeight(body)
+                
+                window.scrollBy(0, step)
+                count += step
+                
+                if (count >= availableScrollHeight) {
+                    clearInterval(intervalId)
+                    resolve(count)
                 }
-            }, speeds);
-        });
-    });
+            }, delay)
+        })
+        return position
+    },
+    scrollStep,
+    scrollDelay,
+    )
+    return lastPosition
 }
