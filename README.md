c7de81af751ff57600cfcd7c67816c2cc5e474a6
d4rkd0s <d4rkd0s@d4rkd0s.com>
2019-08-06 21:59:20 -0700

    Added initial version that just loads the images in search

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # scrapehaven
 Downloads full res images from wallhaven.cc with puppeteer
+
+## Setup
+
+Clone the repo
+
+`git clone https://github.com/d4rkd0s/scrapehaven`
+
+Change into the repo directory
+
+`cd scrapehaven/`
+
+Run NPM install
+
+`npm install`
+
+Copy the example env to a .env
+
+`cp .env.example .env`
+
+Fill out the `.env` file
+
+Open a text editor and change .env to contain your login details
+
+## Use
+
+To search and download for a term run
+
+`node index.js --search="what you want to search"`
+
+More options like sorting, SFW levels, how many to download are coming soon.
---
+++ index.js
@@ -0,0 +1,53 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    require('dotenv').config({path: __dirname + '/.env'})
+    const argv = require('yargs').argv
+
+    function delay(timeout) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, timeout);
+        });
+    }
+    
+    if(!argv.search) { console.log("Please pass a search term with --search=thingtosearch"); process.exit(0); }
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://wallhaven.cc/login');
+    await page.type('#username', process.env.WALLHAVEN_USERNAME);
+    await page.type('#login > input[type=password]:nth-child(3)', process.env.WALLHAVEN_PASSWORD);
+    await page.click('#login > p:nth-child(4) > button');
+    await page.click('#header-search-text');
+    await page.type('#header-search-text', argv.search);
+    await page.click('#header-search > button');
+    const nodeChildren = await page.$eval('#thumbs > section > ul', (uiElement) => {
+        return uiElement.children;
+    });
+    console.log(nodeChildren); // Outputs the array of the nodes children
+    nodeChildren.forEach(child => {
+        console.log(child.src);
+    });
+
+    console.log("Closing in 3 seconds");
+    await delay(3000);
+    await browser.close();
+})();
+
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 1000;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
